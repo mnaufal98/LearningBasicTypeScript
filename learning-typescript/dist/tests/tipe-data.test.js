7d5bd1426def0174f13e01d2198ac554c43cb17d
Muhammad Naufal <mnaufal98@gmail.com>
2023-12-04 08:30:24 +0700

    test

---
+++ learning-typescript/dist/tests/tipe-data.test.js
@@ -0,0 +1,14 @@
+"use strict";
+describe('Data Type', function () {
+    it('should must declare', function () {
+        let name = "naufall";
+        let balance = 1000000;
+        let isVip = true;
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+        // name = 100;
+        // balance = "eko";
+        // isVip = 100;
+    });
+});
